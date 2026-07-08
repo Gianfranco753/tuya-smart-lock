94062b6ca653d7cfa63a63ac769e2cdb30e585df
Gianfranco Cossani Morales <Gianfranco_753@yahoo.es>
2026-07-08 03:04:44 -0400

    feat: error handling
    * feat: handle connection errors gracefully

--- custom_components/tuya_smart_lock/lock.py
+++ custom_components/tuya_smart_lock/lock.py
@@ -8,12 +8,14 @@ import voluptuous as vol
 from homeassistant.components.lock import LockEntity
 from homeassistant.config_entries import ConfigEntry
 from homeassistant.core import HomeAssistant
-from homeassistant.exceptions import HomeAssistantError
+from homeassistant.exceptions import ConfigEntryNotReady, HomeAssistantError
 from homeassistant.helpers import entity_platform
 from homeassistant.helpers.entity_platform import AddEntitiesCallback
 from homeassistant.util import dt as dt_util
 
 from .const import CONF_DEVICE_ID, CONF_DEVICE_NAME, DOMAIN
+
+from .tuya_api import TuyaApiError
 
 _LOGGER = logging.getLogger(__name__)
 
@@ -33,7 +35,11 @@ async def async_setup_entry(
     device_name = entry_data[CONF_DEVICE_NAME]
 
     # Read auto_lock_time from device
-    auto_lock_time = await api.async_get_auto_lock_time(device_id)
+    try:
+        auto_lock_time = await api.async_get_auto_lock_time(device_id)
+    except (TuyaApiError, ConnectionError) as err:
+        raise ConfigEntryNotReady(f"Cannot reach Tuya Cloud API: {err}") from err
+
     if auto_lock_time is None:
         auto_lock_time = DEFAULT_AUTO_LOCK_DELAY
 
@@ -81,7 +87,12 @@ class TuyaSmartLock(LockEntity):
         self._attr_is_locking = True
         self.async_write_ha_state()
 
-        success = await self._api.async_lock(self._device_id)
+        try:
+            success = await self._api.async_lock(self._device_id)
+        except (TuyaApiError, ConnectionError) as err:
+            self._attr_is_locking = False
+            self.async_write_ha_state()
+            raise HomeAssistantError(f"Could not lock the door: {err}") from err
 
         self._attr_is_locking = False
         if success:
@@ -93,7 +104,12 @@ class TuyaSmartLock(LockEntity):
         self._attr_is_unlocking = True
         self.async_write_ha_state()
 
-        success = await self._api.async_unlock(self._device_id)
+        try:
+            success = await self._api.async_unlock(self._device_id)
+        except (TuyaApiError, ConnectionError) as err:
+            self._attr_is_unlocking = False
+            self.async_write_ha_state()
+            raise HomeAssistantError(f"Could not unlock the door: {err}") from err
 
         self._attr_is_unlocking = False
         if success:
@@ -118,10 +134,13 @@ class TuyaSmartLock(LockEntity):
         now = dt_util.utcnow()
         effective_time = int(now.timestamp())
         invalid_time = int((now + timedelta(hours=duration_hours)).timestamp())
-
-        success = await self._api.async_create_temp_password(
-            self._device_id, code, name, effective_time, invalid_time
-        )
-
+        
+        try:
+            success = await self._api.async_create_temp_password(
+                self._device_id, code, name, effective_time, invalid_time
+            )
+        except (TuyaApiError, ConnectionError) as err:
+            raise HomeAssistantError(f"Could not create temporary password '{name}': {err}") from err
+        
         if not success:
             raise HomeAssistantError(f"Failed to create temporary password '{name}'")
